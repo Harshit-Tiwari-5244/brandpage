e2517b39a9028d4b90ba6cc4ecb58adfb244d0a0
Harshit-Tiwari-5244 <147434248+Harshit-Tiwari-5244@users.noreply.github.com>
2024-11-08 12:16:03 +0530

    updated

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+const HeroSection = () => {
+    return <main className="Hero">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>
+                YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+        </div>
+        <div className="hero-image"></div>
+    </main>
+}
+
+export default HeroSection;
