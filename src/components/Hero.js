3c64040dbf081942de5549c055014e9164e70343
Harshit-Tiwari-5244 <147434248+Harshit-Tiwari-5244@users.noreply.github.com>
2024-11-08 12:40:31 +0530

    second update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,21 @@ const HeroSection = () => {
             <h1>YOUR FEET DESERVE THE BEST</h1>
             <p>
                 YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
-                </p>
+            </p>
+            <div className="hero-btn">
+                <button>Shop-Now</button>
+                <button>Category</button>
+            </div>
+            <div className="shopping">
+                <p>Also Available On</p>
+                <img src="amazon.png" alt="amazon-logo" />
+                <img src="flipkart.png" alt="flipkart-logo" />
+            </div>
+
         </div>
-        <div className="hero-image"></div>
+        <div className="hero-image">
+        <img src="shoe_image.png" alt="shoe-image" />
+        </div>
     </main>
 }
 
